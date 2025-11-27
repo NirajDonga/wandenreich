09f4f522fe3abeb3f3641d90742654e7a52a840e
NirajDonga <donganiraj180@gmail.com>
2025-11-27 13:17:11 +0530

    final

--- lib/models/index.ts
+++ lib/models/index.ts
@@ -1,17 +1,4 @@
 // Export all models for easy import
 export { default as User } from './User';
-export { default as UnitOfMeasure } from './UnitOfMeasure';
-export { default as Product } from './Product';
-export { default as ProductVariant } from './ProductVariant';
 export { default as Supplier } from './Supplier';
 export { default as Customer } from './Customer';
-export { default as Inventory } from './Inventory';
-export { default as InventoryTransaction } from './InventoryTransaction';
-export { default as PurchaseOrder } from './PurchaseOrder';
-export { default as PurchaseOrderItem } from './PurchaseOrderItem';
-export { default as SalesOrder } from './SalesOrder';
-export { default as SalesOrderItem } from './SalesOrderItem';
-export { default as CustomerPayment } from './CustomerPayment';
-export { default as SupplierPayment } from './SupplierPayment';
-export { default as CustomerLedger } from './CustomerLedger';
-export { default as SupplierLedger } from './SupplierLedger';
